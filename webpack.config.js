628d4749462e250beedb1c7f9549e1ce2e8777b7
Thomas Godwin <leddfoot36@gmail.com>
2021-09-21 14:13:24 +0200

    Changed page Tab and added a favicon

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin ({
-            title: 'Development',
+            title: 'Oh SH!T! Vinmonopolet',
         })
     ],  
   output: {
